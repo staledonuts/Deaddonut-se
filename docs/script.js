017c0553d8932d7fc8dd5ede277ecc72261665b0
Staledonuts <goblinburrow@gmail.com>
2023-06-22 08:00:16 +0200

    picture?

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(e) {
    var faviconnumber = 1;
-	function favicon() {
+	function favicon() 
+   {
 		favicon = favicon == 1 ? 2 : 1;
 		$('.favicon').attr('href','favicon' + favicon + ".png");
 	}
@@ -38,7 +39,8 @@ $(document).ready(function(e) {
             A! - spaces are converted to non-breaking spaces (it's for ascii art - after all, this is a text based website)
       */
                                                        
-   function init() {
+   function init() 
+   {
       setInterval(time);
       console.clear();
       console.log(new Date().getTime());
@@ -50,50 +52,66 @@ $(document).ready(function(e) {
       log("Website", "A!╚═════╝ ╚══════╝╚═╝  ╚═╝╚═════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝    ╚═╝   ");
       log("Website", "Welcome to "+ '[^http://www.Deaddonut.se/](*Deaddonut.se*)');
       log("Website", "This is Erik Lindahls Portfolio page.");
-      log("Website", "");
+      log("Website", picture(deaddonut.png));
 	  urlvars();
       log("Client", "For help input '/help'");
 	  setInterval(favicon,500);
    }
 
-   function urlvars() {
+   function picture(imagename)
+   {
+      var img = $('<img />', {src : 'https://www.deaddonut.se/images/' + $(imagename).val() +'.png'});
+   }
+
+   function urlvars() 
+   {
 	   var pagelocs = window.location.pathname.replace("/","").split("/");
 	   var pageloc = pagelocs[0];
 	   console.log(pageloc);
 	   //alert();
 		if(pageloc != "") {
-            if ($.inArray(pageloc, pageindex) >= 0) {
+            if ($.inArray(pageloc, pageindex) >= 0) 
+            {
                currentpage = pageloc;
             }
 		}
       	log("Website", "You are currently on page: *" + currentpage + "*");
 		if(pageloc != "") {
-            if ($.inArray(pageloc, pageindex) >= 0) {
+            if ($.inArray(pageloc, pageindex) >= 0) 
+            {
                currentpage = pageloc;
                loadpage($.inArray(pageloc, pageindex));
-            } else {
+            } 
+            else 
+            {
                //Un-note next line to show 404 errors with wrong urls
                //log("Client", "404 - The page '" + pageloc + "' does not exist. To "); 
             }
 		}
-		if(pageloc == "") {
+		if(pageloc == "") 
+      {
       		log("Client", "What would you like to access?");	
 		}
    }
-   function getParam(name){
+
+   function getParam(name)
+   {
 		name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
 		var regexS = "[\\?&]"+name+"=([^&#]*)";
 		var regex = new RegExp( regexS );
 		var results = regex.exec (window.location.href);
-		if (results == null) {
+		if (results == null) 
+      {
 			return "";
 		}
-		else  {
+		else  
+      {
 			return results[1];
 		}
 	}
 
-   function log(name, information) {
+   function log(name, information) 
+   {
       var d = new Date();
       var hours = ((d.getHours() < 10) ? "0" : "") + d.getHours();
       var minutes = ((d.getMinutes() < 10) ? "0" : "") + d.getMinutes();
@@ -102,13 +120,15 @@ $(document).ready(function(e) {
       var textcolour = "";
       var postcolour = "";
 
-      switch (name[0]) {
+      switch (name[0]) 
+      {
          case "!":
             postcolour = " important";
             name = name.substr(1);
             break;
       }
-      switch (name) {
+      switch (name) 
+      {
          case "Website":
             colour = "redt";
             break;
@@ -123,16 +143,19 @@ $(document).ready(function(e) {
             postcolour = " selft";
             break;
       }
-      if (information[0] == "A" && information[1] == "!") {
+      if (information[0] == "A" && information[1] == "!") 
+      {
          information = information.substr(2);
          information = information.replace(/ /g, '\u00A0');
       }
-      if (information[0] == "E" && information[1] == "!") {
+      if (information[0] == "E" && information[1] == "!") 
+      {
          information = information.substr(2);
          postcolour = " important";
       }
 
-      while (information.indexOf("](") >= 0) { //URL parser
+      while (information.indexOf("](") >= 0) 
+      { //URL parser
 
          var NAMEregExp = /\(([^)]+)\)/;
          var uname = NAMEregExp.exec(information)[1];
@@ -140,15 +163,19 @@ $(document).ready(function(e) {
          var URLregExp = /\[([^)]+)\]/;
          var url = URLregExp.exec(information)[1];
          var newpage = false;
-         if (url[0] == "^") {
+         if (url[0] == "^") 
+         {
             newpage = true;
             url = url.substr(1);
          }
          var start = information.indexOf("[");
          var end = information.indexOf(")");
-         if (newpage) {
+         if (newpage) 
+         {
             information = information.replace(information.substring(start, end + 1), "").splice(start, 0, '<a href="' + url + '" target="_blank">' + uname + '</a>');
-         } else {
+         } 
+         else 
+         {
             information = information.replace(information.substring(start, end + 1), "").splice(start, 0, '<a href="' + url + '">' + uname + '</a>');
          }
          //information = '<a href="' + url + '">' + uname + '</a>'; //working
@@ -156,29 +183,36 @@ $(document).ready(function(e) {
       }
       var tobold = true;
       var boldnumber = 0;
-      for (var i = 0; i < information.length; i++) {
-         if (information[i] == "*" && information[i - 1] != "*" && information[i + 1] != "*") {
+      for (var i = 0; i < information.length; i++) 
+      {
+         if (information[i] == "*" && information[i - 1] != "*" && information[i + 1] != "*") 
+         {
             boldnumber++;
          }
       }
-      while (information.indexOf("*") >= 0) { //Bold parser
+      while (information.indexOf("*") >= 0) 
+      { //Bold parser
          var pos = information.indexOf("*");
          information = information.replace("*", "");
-         if (tobold) {
+         if (tobold) 
+         {
             information = information.splice(pos, 0, '<b>');
          } else {
             information = information.splice(pos, 0, '</b>');
          }
          tobold = !tobold;
-         if (tobold && boldnumber <= 1) {
+         if (tobold && boldnumber <= 1) 
+         {
             break;
          }
          //information = '<a href="' + url + '">' + uname + '</a>'; //working
       }
       var tounderline = true;
       var underlinenumber = 0;
-      for (var i = 0; i < information.length; i++) {
-         if (information[i] == "*" && information[i - 1] != "*" && information[i + 1] != "*") {
+      for (var i = 0; i < information.length; i++) 
+      {
+         if (information[i] == "*" && information[i - 1] != "*" && information[i + 1] != "*") 
+         {
             underlinenumber++;
          }
       }
@@ -191,7 +225,8 @@ $(document).ready(function(e) {
             information = information.splice(pos, 0, '</u>');
          }
          tounderline = !tounderline;
-         if (tounderline && underlinenumber <= 1) {
+         if (tounderline && underlinenumber <= 1) 
+         {
             break;
          }
          //information = '<a href="' + url + '">' + uname + '</a>'; //working
@@ -204,35 +239,43 @@ $(document).ready(function(e) {
       $(document).scrollTop($(document).height() - $(window).height());
    }
 	var timestring = "";
-   function time() {
+   function time() 
+   {
       var d = new Date();
       var hours = d.getHours();
       var minutes = d.getMinutes();
       var seconds = d.getSeconds();
-      if (hours < 10) {
+      if (hours < 10) 
+      {
          hours = "0" + hours;
       }
-      if (minutes < 10) {
+      if (minutes < 10) 
+      {
          minutes = "0" + minutes;
       }
-      if (seconds < 10) {
+      if (seconds < 10) 
+      {
          seconds = "0" + seconds;
       }
 	  var temptimestring = "[" + hours + ":" + minutes + ":" + seconds + "]";
-	  if (temptimestring != timestring) {
+	  if (temptimestring != timestring) 
+     {
 		  timestring = temptimestring;
       	$(".editline .time").text(timestring);
 	  }
    }
 
    var ctrldown = false;
-   $(".editline .edit").keydown(function(e) {
+   $(".editline .edit").keydown(function(e) 
+   {
       var text = $(".editline .edit").text();
       console.log(e.which);
-      if (e.which == 13 && text !== "" && !ctrldown) {
+      if (e.which == 13 && text !== "" && !ctrldown) 
+      {
          var commands = text.split(' ');
          var output = "";
-         if (commands[0] == "help") {
+         if (commands[0] == "help") 
+         {
             text = "/" + text;
          }
          $(".editline .edit").text("");
@@ -248,25 +291,29 @@ $(document).ready(function(e) {
 
       }
       if (e.which == 38) { //up
-         if (currentcommand > 0) {
+         if (currentcommand > 0) 
+         {
             currentcommand--;
             $(".editline .edit").text(previouscommands[currentcommand]);
          }
       }
       if (e.which == 40) { //down
 
-         if (currentcommand < previouscommands.length) {
+         if (currentcommand < previouscommands.length) 
+         {
             currentcommand++;
             $(".editline .edit").text(previouscommands[currentcommand]);
          }
       }
    });
 
-   function cmd(command, words, word) {
+   function cmd(command, words, word) 
+   {
       switch (word[0]) {
          case "/help":
          case "help":
-            for (var i = 0; i < commandlist.length; i++) {
+            for (var i = 0; i < commandlist.length; i++) 
+            {
                output = commandlist[i][0] + " : " + commandlist[i][1];
                //console.log(command[i][0]);
                log("Client", output);
@@ -280,16 +327,20 @@ $(document).ready(function(e) {
             $(".stream").text("");
             break;
          case "/nav":
-            if ($.inArray(word[1], pageindex) >= 0) {
+            if ($.inArray(word[1], pageindex) >= 0) 
+            {
                currentpage = word[1];
                log("Website", "You are now in " + currentpage);
                loadpage($.inArray(word[1], pageindex));
-            } else {
+            } 
+            else 
+            {
                log("Client", "'" + word[1] + "' does not exist.");
             }
             break;
          case "/list":
-            $.each(pageindex, function(id, content) {
+            $.each(pageindex, function(id, content) 
+            {
                log("Client", "> " + content);
             });
             break;
@@ -298,12 +349,15 @@ $(document).ready(function(e) {
                log("Client", "> " + "Found at [^https://int10h.org/](*int10h.org*), created by some wonderful person for the 'IBM MCGA and VGA (PS/2) video BIOS'");
             break;
          case "/login":
-            if (word.length >= 3) {
+            if (word.length >= 3) 
+            {
                log("Client", "Attempting to login to " + word[1] + " with " + Array(word[2].length + 1).join("â€¢"));
                loginreturn = false;
                //log("Client", "ER1");
                setTimeout(loginemptyreturn, 20000);
-            } else {
+            } 
+            else 
+            {
                log("Client", "Not enough arguments to log in, you need a USERNAME and a PASSWORD.");
             }
             break;
@@ -314,21 +368,25 @@ $(document).ready(function(e) {
    }
 
    function loadpage(i) {
-      $.each(pages[i], function(id, content) {
-         if (content != pageindex[i]) {
+      $.each(pages[i], function(id, content) 
+      {
+         if (content != pageindex[i]) 
+         {
             log("Website", content);
          }
       });
    }
    var loginreturn = false;
 
-   function loginemptyreturn() {
+   function loginemptyreturn() 
+   {
       //log("Client", "ER2");
       if (!loginreturn) {
          log("Client", "E![LOGIN] No Return Recieved");
       }
    }
-   String.prototype.splice = function(idx, rem, str) {
+   String.prototype.splice = function(idx, rem, str) 
+   {
       return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
    };
    init();
