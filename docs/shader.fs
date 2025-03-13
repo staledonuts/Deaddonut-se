ce328d791eddb08ff7c52836552e9f2f6611029a
Erik Lindahl <goblinburrow@gmail.com>
2025-03-13 22:35:34 +0100

    shader update

--- docs/shader.fs
+++ docs/shader.fs
@@ -16,8 +16,48 @@ vec4 overlay(vec4 base, vec4 overlay, float overlayAlpha)
 
 vec2 SnapUV(vec2 uv)
 {
-    const float gridSize = 0.001;
+    const float gridSize = 0.01;
     return floor(uv / gridSize) * gridSize;
+}
+
+float OutBounce(float t)
+{
+    float div = 2.75;
+    float mult = 7.5625;
+
+    if (t < 1.0 / div)
+    {
+        return mult * t * t;
+    }
+    else if (t < 2.0 / div)
+    {
+        t -= 1.5 / div;
+        return mult * t * t + 0.75;
+    }
+    else if (t < 2.5 / div)
+    {
+        t -= 2.25 / div;
+        return mult * t * t + 0.9375;
+    }
+    else
+    {
+        t -= 2.625 / div;
+        return mult * t * t + 0.984375;
+    }
+}
+
+float InBounce(float t)
+{
+    return 1.0 - OutBounce(1.0 - t);
+}
+
+float InOutBounce(float t)
+{
+    if (t < 0.5) 
+    {
+        return InBounce(t * 2.0) / 2.0;
+    }
+    return 1.0 - InBounce((1.0 - t) * 2.0) / 2.0;
 }
 
 void main(void) 
