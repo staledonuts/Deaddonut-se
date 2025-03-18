8d39946d8585d6f88f3549c613d900753626cf07
Erik Lindahl <goblinburrow@gmail.com>
2025-03-18 13:11:45 +0100

    removed redefiend uniforms

--- docs/GLCanvas.js
+++ docs/GLCanvas.js
@@ -12,13 +12,6 @@ function init()
     camera.position.z = 2;
     // Vertex Shader
     const vertexShader = `
-        attribute vec3 position;
-        attribute vec3 normal;
-        attribute vec2 uv;
-
-        uniform mat4 projectionMatrix;
-        uniform mat4 modelViewMatrix;
-
         varying vec2 vUv;
         varying vec3 vNormal;
         varying vec3 vPosition;
